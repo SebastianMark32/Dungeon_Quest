2f2ec22671fa23f341af7b6e576f778e38e6dd58
SebastianMark32 <smarkgolfer@gmail.com>
2023-04-04 13:54:50 -0400

    Removed and changed CMake
    Removed a .png file that was not being used. Also, I changed the current CMake file so it works with the current directory.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/sebastianmark/Desktop/SFML_D_Session
+# Install script for directory: /Users/sebastianmark/Desktop/Dungeon_Quest
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/sebastianmark/Desktop/SFML_D_Session/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/sebastianmark/Desktop/Dungeon_Quest/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
