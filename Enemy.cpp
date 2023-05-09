bdf5045fab1e599a12a3dec018f7994470e4ddd9
SebastianMark32 <smarkgolfer@gmail.com>
2023-05-08 20:51:19 -0400

    Enemy Killing Progress

--- Enemy.cpp
+++ Enemy.cpp
@@ -22,13 +22,8 @@ sf::Sprite * Enemy::getSprite() {
     return &this->sprite;
 }
 
-void Enemy::setSprite(std::string path) {
-    this->texture.loadFromFile(path);
-    this->sprite.setTexture(texture);
-}
-
 bool Enemy::isAlive(){
-    return this->alive;
+    return this->alive = true;
 }
 void Enemy::set_Alive(bool alive) {
     this->alive = alive;
@@ -86,5 +81,3 @@ int Enemy::getCurrentTile() {
 void Enemy::setCurrentTile(int currentTile) {
     this->currentTile = currentTile;
 }
-
-
